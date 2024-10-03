f45ce0ca92b8904c6f52ddeb8884cb6f0b031efc
abhiInLunara <abhishek.yadav@lunarasolutions.in>
2024-10-03 07:57:24 +0530

    select view

--- src/component/Step2.jsx
+++ src/component/Step2.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import borad from "../../public/images/board.png";
+// import list from "../../public/images/list.png";
+// import skf from '../'
 
-const Step2 = () => {
+const Step2 = ({ handleChange, formData }) => {
+  const [selectedView, setSelectedView] = useState("list");
+  useEffect(() => {
+    handleChange({ target: { name: "view", value: selectedView } });
+  }, [selectedView]);
   return (
-    <div className="w-full flex flex-col gap-3 items-center justify-center mb-4">
-      <h1 className="text-center  text-2xl font-semibold">Create a project</h1>
-      <p className="">You can also customize this views in settings</p>
-      <div></div>
+    <div className="w-full flex flex-col gap-1 items-center justify-center mb-4">
+      <h1 className="text-center  text-2xl font-semibold">Select a view</h1>
+      <p className="text-[14px] font-normal text-gray-400">
+        You can also customize this views in settings
+      </p>
+      <div className=" w-full justify-between gap-5 items-center flex mt-8">
+        <div
+          className={`w-[50%] border ring-1 p-3 ring-gray-300 ${
+            selectedView === "list" && "outline-none ring-1 ring-blue-500"
+          } rounded-md`}
+          onClick={() => setSelectedView("list")}
+        >
+          <img
+            className="h-[120px] w-full contain"
+            src="./list.png"
+            alt="list image"
+          />
+        </div>
+        <div
+          className={`w-[50%] border ring-1 p-3 ring-gray-300 ${
+            selectedView === "board" && "outline-none ring-1 ring-blue-500"
+          } rounded-md`}
+          onClick={() => setSelectedView("board")}
+        >
+          <img src="" className="h-[120px] w-full contain" alt="Board image" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/CreateProject.jsx
+++ src/pages/CreateProject.jsx
@@ -56,57 +56,61 @@ const CreateProject = () => {
   };
   return (
     <div className="w-screen h-screen flex  items-center justify-center">
-      <div className="w-[80%] md:w-[600px] relative rounded-md pl-12 pr-12  shadow-md border border-b-2 flex-col flex pt-10">
-        <span className="absolute right-2 top-3">
-          <Link to="/">
-            <RxCross2 size={18} />
-          </Link>
-        </span>
-        {cureentWindow()}
-        <div className="w-full flex  justify-center items-center relative">
-          {currentStep > 1 ? (
-            <button className="absolute left-0" onClick={prevStep}>
-              <FaLessThan className="text-gray-400 inline  h-3 w-3" />
-              <span className="text-gray-400"> Back</span>
-            </button>
-          ) : (
-            <Link to="/" className="absolute left-0">
-              <FaLessThan className="text-gray-400 inline  h-3 w-3" />
-              <span className="text-gray-400"> Back</span>
+      <div className="w-[80%] min-h-[600px] md:w-[600px] relative rounded-md pl-12 pr-12  shadow-md border border-b-2 flex-col flex pt-10 justify-between">
+        <div>
+          <span className="absolute right-2 top-3">
+            <Link to="/">
+              <RxCross2 size={18} />
             </Link>
-          )}
-
-          {(currentStep > 1 && currentStep) < 4 && (
-            <button
-              className="bg-blue-400 text-white w-fit  text-noraml px-6 py-1 rounded-md"
-              onClick={nextStep}
-            >
-              Next
-            </button>
-          )}
-          {currentStep === 4 && (
-            <button
-              className="bg-blue-400 text-white w-fit  text-noraml px-6 py-1 rounded-md"
-              onClick={handleSubmit}
-            >
-              Submit
-            </button>
-          )}
+          </span>
+          {cureentWindow()}
         </div>
+        <div className="">
+          <div className="w-full flex  justify-center items-center relative">
+            {currentStep > 1 ? (
+              <button className="absolute left-0" onClick={prevStep}>
+                <FaLessThan className="text-gray-400 inline  h-3 w-3" />
+                <span className="text-gray-400"> Back</span>
+              </button>
+            ) : (
+              <Link to="/" className="absolute left-0">
+                <FaLessThan className="text-gray-400 inline  h-3 w-3" />
+                <span className="text-gray-400"> Back</span>
+              </Link>
+            )}
 
-        {/* paginations dots */}
-        <div className="flex gap-1 justify-center items-center p-3 my-4">
-          {Array.from({ length: totalPages }).map((_, index) => (
-            <span
-              key={index}
-              className={`h-2  rounded-full cursor-pointer ${
-                currentStep === index + 1
-                  ? "bg-gray-500 w-4 "
-                  : "bg-gray-300 w-2"
-              }`}
-              onClick={() => setCurrentStep(index + 1)}
-            ></span>
-          ))}
+            {(currentStep > 1 && currentStep) < 4 && (
+              <button
+                className="bg-blue-400 text-white w-fit  text-noraml px-6 py-1 rounded-md"
+                onClick={nextStep}
+              >
+                Next
+              </button>
+            )}
+            {currentStep === 4 && (
+              <button
+                className="bg-blue-400 text-white w-fit  text-noraml px-6 py-1 rounded-md"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+            )}
+          </div>
+
+          {/* paginations dots */}
+          <div className="flex gap-1 justify-center items-center p-3 my-4">
+            {Array.from({ length: totalPages }).map((_, index) => (
+              <span
+                key={index}
+                className={`h-2  rounded-full cursor-pointer ${
+                  currentStep === index + 1
+                    ? "bg-gray-500 w-4 "
+                    : "bg-gray-300 w-2"
+                }`}
+                onClick={() => setCurrentStep(index + 1)}
+              ></span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
